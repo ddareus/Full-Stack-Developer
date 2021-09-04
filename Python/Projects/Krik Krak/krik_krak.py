c576bdef4ae1a05aa874ae45fcee93d2b3d446e0
Daniel Daréus <dareusdaniel@gmail.com>
2021-09-04 01:01:08 -0400

    Haitians tell a story

---
+++ Python/Projects/Krik Krak/krik_krak.py
@@ -0,0 +1,62 @@
+while True:
+    # Add the answer
+    answer = 'chyen'
+    # print(answer) #cheating
+    # Input from user
+    guess = input('Lè m chita, mwen pi wo: ')
+    guess = guess.lower()
+    # Check if input is the right guess.
+    if guess == answer:
+        print('Bravo. Ou jwenn bon repons lan!')
+        break
+        # If the answer is not the right one, print this message
+    else:
+        print('Repons ou antre an pa bon. Eseye ankò.')
+        continue
+
+while True:
+    answer_1 = 'bale'
+    # print(answer) #cheating
+    # Input from user
+    guess_1 = input('Kaptèn dèyè pòt: ')
+    guess_1 = guess_1.lower()
+    # Check if input is the right guess.
+    if guess_1 == answer_1:
+        print('Bravo. Ou jwenn bon repons lan!')
+        break
+        # If the answer is not the right one, print this message
+    else:
+        print('Repons ou antre an pa bon. Eseye ankò.')
+        continue
+
+answer_2 = 'kabann'
+
+while True:
+    # print(answer) #cheating
+    # Input from user
+    guess_2 = input('Abiye san sòti: ')
+    guess_2 = guess_2.lower()
+    # Check if input is the right guess.
+    if guess_2 == answer_2:
+        print('Bravo. Ou jwenn bon repons lan!')
+        break
+        # If the answer is not the right one, print this message
+    else:
+        print('Repons ou antre an pa bon. Eseye ankò.')
+        continue
+
+answer_3 = 'zye'
+
+while True:
+    # print(answer) #cheating
+    # Input from user
+    guess_3 = input('Fizi kout tire lwen: ')
+    guess_3 = guess_3.lower()
+    # Check if input is the right guess.
+    if guess_3 == answer_3:
+        print('Bravo. Ou jwenn bon repons lan!')
+        break
+        # If the answer is not the right one, print this message
+    else:
+        print('Repons ou antre an pa bon. Eseye ankò.')
+        continue
